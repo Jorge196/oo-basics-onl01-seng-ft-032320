dc72b01fdc47f15c51f6f45b7ecd6e484baa4227
Jorge Ramirez <joramirez96@gmail.com>
2020-04-02 18:22:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,38 +1,3 @@
 class Book
 
-#   def initialize(title)
-#     @title = title
-#   end
-#
-#   def title
-#     @title
-#   end
-#
-#   def author=(author)
-#     @au
-#   end
-#
-#   def
-#   end
-#
-#   def page_count=(num)
-#     @page_count = num
-#   end
-#
-#   def page_count
-#     @page_count
-#   end
-#
-#   def genre=(genre)
-#     @genre = genre
-#   end
-#
-#   def genre
-#     @genre
-#   end
-#
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
-#
-# end
+  attr_accessor :author, :page_counter, :genre
